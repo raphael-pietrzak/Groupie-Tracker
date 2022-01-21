dde08957873d2264871dfd8550b7864d944c7c22
Max Doublait <maxdoublait@Maxs-MacBook-Pro.local>
2022-01-21 10:53:05 +0100

    html

--- mainpage.go
+++ mainpage.go
@@ -11,6 +11,8 @@ type Artistes struct{
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("static/index.html"))
-	new := Artistes()
-	tmpl.Execute(w, new)
+	data := ArtistStruct{
+		Tab: ArtistsTab,
+	}
+	tmpl.Execute(w, data)
 }
