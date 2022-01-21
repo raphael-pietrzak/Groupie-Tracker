0b0cd53ada9589da2c4b35580f00d0d7244e51bf
RPIETRZAK <raphael.pietrzak@ynov.com>
2022-01-21 11:15:54 +0100

    f commit

--- api.go
+++ api.go
@@ -1,29 +1,46 @@
 package groupie
 
 import (
-	"enconding/json"
 	"fmt"
 	"io/ioutil"
-	"io/outil"
 	"net/http"
 )
 
-type ArtistStruct struct{
-	tab []Artist 
+type ArtistStruct struct {
+	tab []Artist
 }
 
 type Artist struct {
-	Id int `json:"id"`
-	Image string `json:"image"`
-	Name string `json:"name"`
-	Members []string `json:"members"`
-	CreationDate int `json:"creationDate"`
-	FirstAlbum string `json:"firstAlbum"`
-	
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
 }
 
-func APIRequests (){
-	
+type Locations struct {
+	Id        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
+}
+
+type Date struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+type DatesLocations struct {
+	Location string   `json:"locations"`
+	Date     []string `json:"dates"`
+}
+
+type Relations struct {
+	Id             int            `json:"id"`
+	Dateslocations DatesLocations `json:"dateslocations"`
+}
+
+func APIRequests() {
+
 	req, err := http.Get("https://groupietrackers.herokuapp.com/api")
 
 	if err != nil {
--- mainpage.go
+++ mainpage.go
@@ -8,8 +8,7 @@ import (
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("static/index.html"))
-	new := 
-	new := Artistes{a: 3,b: "rjfn", c:[]string{"fzer"}}
+	new = Artistes{a: 3,b: "rjfn", c:[]string{"fzer"}}
 	tmpl.Execute(w, new)
 
 }
