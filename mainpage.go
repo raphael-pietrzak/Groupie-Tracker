c43f751cb3dc931b3fbfc9cfeafdad76463a8db9
RPIETRZAK <raphael.pietrzak@ynov.com>
2022-01-21 10:40:43 +0100

    pull

--- mainpage.go
+++ mainpage.go
@@ -6,11 +6,13 @@ import (
 )
 
 type Artistes struct{
-	
+	a int
+	b string
+	c []string
 }
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("static/index.html"))
-	new := Artistes()
+	new := Artistes{a: 3,b: "rjfn", c: ["kjsdfh"]}
 	tmpl.Execute(w, new)
 }
